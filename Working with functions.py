cee19aacf0d8d0c2141a9663093f06bbb866f595
aaryamantriescode <163023807+aaryamantriescode@users.noreply.github.com>
2024-03-17 20:40:18 +0530

    commit2

--- Working with functions.py
+++ Working with functions.py
@@ -9,3 +9,19 @@ num2 = float(input("Enter another number: "))
 result = calcsum(num1, num2)  # Call the function and store the result
 
 print("Sum:", result)  # Print the result
+
+#write a function Lshift(arr,n)in python, which accepts a list arr of numbers 
+#and n is the numeric value by which all the elements of the list are shifted to left 
+
+def Lshift(Arr,n):
+    L=len(Arr)
+    for x in range(0,n):
+        y=Arr[0]
+        for i in range(L-1):
+            Arr[i]=Arr[i+1]
+        Arr[L-1]=y
+        return Arr
+Arr=[10,20,30,40,12,11]
+n=2
+result=Lshift(Arr,n)
+print(result)
