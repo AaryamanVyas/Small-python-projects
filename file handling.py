12a2361ec6decc3f1f0f003131ab8d10d9141aef
aaryamantriescode <163023807+aaryamantriescode@users.noreply.github.com>
2024-03-20 22:26:51 +0530

    making new file learning how to read a file with python learning how to write in a file with python

--- file handling.py
+++ file handling.py
@@ -20,5 +20,44 @@ print("number of lines are :",linecount)
 myfile.close()
 
 
+#basic program on how to write into text files
 
+#write a program to get roll numbers, names and marks of the students of a class(get from user)
+# and store these details in a dile called "marks".txt
+count=int(input("enter how many students inside the class"))
+fileout=open("marks.txt",'w')
+for i in range(count):
+    print("enter details for student",(i+1),"below:")
+    rollno=int(input("rolno:"))
+    name=input("name:")
+    marks=float(input("marks:"))
+    rec = f"{rollno},{name},{marks}\n"
+    fileout.write(rec)
+fileout.close()
+
+#write a program to read a text file line by line and display each word seperated by a "#"
+myfile=open("marks.txt",'r')
+while str:
+    str=myfile.readline()
+    print(str)
+myfile.close()
 #
+# #write a program to read a text file and display the count of vovels ans consonants in the file
+file = open("marks.txt", 'r')
+content = file.read()  # Read the entire content of the file
+file.close()
+
+vowels = "aeiou"
+vowel_count = 0
+consonant_count = 0
+
+for char in content:
+    if char.isalpha():  # Check if the character is an alphabet
+        if char.lower() in vowels:  # Convert to lowercase for comparison
+            vowel_count += 1
+        else:
+            consonant_count += 1
+
+print("The number of vowels in the file:", vowel_count)
+print("The number of consonants in the file:", consonant_count)
+file.close()
