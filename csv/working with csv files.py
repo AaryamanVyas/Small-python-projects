290658a5402f5a9a13b73d458afab9942dfaec60
aaryamantriescode <163023807+aaryamantriescode@users.noreply.github.com>
2024-03-21 12:21:44 +0530

    making new file learning how to read a file with python learning how to write in a file with python making a new file for pickling and unpickling leaning how to load and dump and ssearch for records learning how to update records removed # csv folder and basic code for csv removed#

--- csv/working with csv files.py
+++ csv/working with csv files.py
@@ -2,32 +2,32 @@
 import csv
 
 # writing in cvs
-#write a program to create a csv file to store student data(rollno,name,marks)obtain data from the user
-#and write 5 records into the file
+# write a program to create a csv file to store student data(rollno,name,marks)obtain data from the user
+# and write 5 records into the file
 
-# file=open("student.csv",'w')
-# writer=csv.writer(file)
-# writer.writerow(['rollno','name','marks'])
-# for i in range(5):
-#     print("student record",(i+1))
-#     rollno=int(input("enter rollno"))
-#     name=input("enter your name")
-#     marks=int(input("enter your marks"))
-#     sturec=[rollno,name,marks]
-#     writer.writerow(sturec)
-# file.close()
+file=open("student.csv",'w')
+writer=csv.writer(file)
+writer.writerow(['rollno','name','marks'])
+for i in range(5):
+    print("student record",(i+1))
+    rollno=int(input("enter rollno"))
+    name=input("enter your name")
+    marks=int(input("enter your marks"))
+    sturec=[rollno,name,marks]
+    writer.writerow(sturec)
+file.close()
 
-#to add data in csv
-# myfile=open("compresult.csv",'w')
-# cswriter=csv.writer(myfile)
-# compdat=[
-#     ['name','points','rank'],
-#     ['shradha',4500,23],
-#     ['nishchay',4800,31],
-#     ['ali',4500,25],
-#     ['name',5100,14]]
-# cswriter.writerow(compdat)
-# myfile.close()
+# to add data in csv
+myfile=open("compresult.csv",'w')
+cswriter=csv.writer(myfile)
+compdat=[
+    ['name','points','rank'],
+    ['shradha',4500,23],
+    ['nishchay',4800,31],
+    ['ali',4500,25],
+    ['name',5100,14]]
+cswriter.writerow(compdat)
+myfile.close()
 
 #to read csv file
 file=open("compresult.csv",'r',newline='\r\n')
