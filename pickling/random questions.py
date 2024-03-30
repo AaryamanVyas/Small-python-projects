321844276910224c9aed5d36ac1f66cb31313c7f
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-03-30 12:28:01 +0530

    question

--- pickling/random questions.py
+++ pickling/random questions.py
@@ -1,1 +1,11 @@
-#
+#write a function in python to count the number of lines in a text file'story.txt' 
+#which are starting with the letter a 
+file=open("STORY.TXT",'r')
+lines=file.readlines()
+count=0
+for w in lines:
+    if w[0]=='A' or w[0]=='a':
+        count+=1
+        print("total lines is :",count)
+        file.close()
+
